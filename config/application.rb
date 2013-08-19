b51ed562ae102cae3cfbcd58a3de34dc8b03b222
Tolomaus <niek.bartholomeus@gmail.com>
2013-08-19 23:26:01 +0200

    Worked on the design of the header in the workingarea

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,6 @@ module Releaseme
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/services)
 
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
