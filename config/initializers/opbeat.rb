33850190ee300751696aebe91b2156005ae3375b
tolomaus <niek.bartholomeus@gmail.com>
2014-06-25 11:04:03 +0200

    Added opbeat error logging

---
+++ config/initializers/opbeat.rb
@@ -0,0 +1,7 @@
+require "opbeat"
+
+Opbeat.configure do |config|
+  config.organization_id="89790f7d3ae942d48ce468782eb47349"
+  config.app_id="020d0f761d"
+  config.secret_token="cb87ba315bc57c52ca2fb5411972c11254b3f225"
+end
